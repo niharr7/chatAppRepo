6a1731edb899f51dd4312c449eec345487880a0d
niharr7 <niharr7@gmail.com>
2017-05-18 17:12:48 +0530

    hackathon commit

---
+++ extension/background.js
@@ -0,0 +1,23 @@
+
+
+            chrome.browserAction.onClicked.addListener(function (tab) {
+                if (!tab.url.indexOf('https://chrome.google.com/') ||
+                    !tab.url.indexOf('chrome://') ||
+                    !tab.url.indexOf('about:') ||
+                    !tab.url.indexOf('chrome-extension://')) {
+                    alert('For security reasons Chrome extensions cannot ' +
+                        'run content scripts at this page.');
+                    
+                    return;
+                }
+                chrome.windows.create({
+                    url: 'Inspector.html#' + tab.windowId,
+                    left: 0,
+                    top: parseInt(screen.availHeight / 2, 10) || 400,
+                    width: parseInt(screen.availWidth, 10) || 800,
+                    height: parseInt(screen.availHeight / 2, 10) || 400,
+                    type: 'popup'
+                });
+            });
+ 
+   
